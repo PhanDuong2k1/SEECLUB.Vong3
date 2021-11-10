f9f9d8c6a79dcd0aa48d4f93e50f8c51dacd2cb6
PhanDuong2k1 <94064041+PhanDuong2k1@users.noreply.github.com>
2021-11-11 01:14:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Bé à!',
     introDesc: `Trái đất vốn lạ thường
-    /n Mà sao em cứ đi nhầm đường
-    /n Lạc vào S.E.E. đây mất rồi
-    /n Đằng sau chữ yêu đây là NHÀ`,
-    btnIntro: 'hihi',
+    , mà sao em cứ đi nhầm đường
+    . Lạc vào S.E.E. đây mất rồi
+    . Đằng sau chữ yêu đây là NHÀ`,
+    btnIntro: 'Hihi',
     title: 'Phải chăng em đã yêu S.E.E ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy chúng tôi nà',
-    btnYes: 'Vẫn cứ là thích S.E.E. <33',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy chúng tôi này',
+    btnYes: 'Vẫn cứ là thích S.E.E. <3',
     btnNo: 'Không :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu S.E.E. <3',
     btnReply: 'Gửi cho S.E.E. <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'S.E.E. biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h lên Gather trò chệ cù S.E.E. nha :v',
+    messDesc: 'Tối nay 7h lên Gather trò chuyện cùng S.E.E. nha :v',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/SEECLUB.YDS' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
