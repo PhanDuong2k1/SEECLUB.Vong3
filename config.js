91a80f894b10baacc588f47cd0ca742f732ad62e
PhanDuong2k1 <94064041+PhanDuong2k1@users.noreply.github.com>
2021-11-11 01:47:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho S.E.E. <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'S.E.E. biết mà 🥰. Yêu bé nhiều nhiều 😘',
-    messDesc: 'Tối nay 7h lên Gather trò chuyện cùng S.E.E. nha :v',
+    messDesc: 'Tối nay 8h lên Gather trò chuyện cùng S.E.E. nha :v',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/SEECLUB.YDS' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
