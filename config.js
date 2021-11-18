33ccb6a39fe204e5eb62cd164524d45d6a49a9bc
Phan Dương <94064041+SEECLUB@users.noreply.github.com>
2021-11-18 19:27:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao BÉ lại yêu S.E.E. <3',
     btnReply: 'Gửi cho S.E.E. <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'S.E.E. xin cảm ơn bạn đã dành thời gian tham chương trình UMP's Diary',
+    mess: 'S.E.E. xin cảm ơn bạn đã dành thời gian tham chương trình UMPs Diary',
     messDesc: 'S.E.E. gửi bạ danh sách tham gia chương trình',
     btnAccept: 'Mở danh sách',
     messLink: 'https://www.facebook.com/SEECLUB.YDS' //link mess của các bạn. VD: https://m.me/nam.nodemy
