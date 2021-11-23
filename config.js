4a4737ab6587a2ba980b32ad11b2e5cfa6cfbee6
Phan Dương <94064041+SEECLUB@users.noreply.github.com>
2021-11-23 16:56:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao BÉ lại yêu S.E.E. <3',
     btnReply: 'Gửi cho S.E.E. <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Cảm ơn bạn đã dành thời gian tham chương trình UMPs Diary',
+    mess: 'Cảm ơn bạn đã tham chương trình UMPs Diary',
     messDesc: 'Bạn gửi lại minh chứng các hoạt động đã làm giúp S.E.E. nha',
     btnAccept: 'Gửi minh chứng',
     messLink: 'https://forms.gle/2woCeJkH6zvmVWi4A' //link mess của các bạn. VD: https://m.me/nam.nodemy
